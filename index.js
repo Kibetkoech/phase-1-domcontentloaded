e83f66bdfae32d09060c29255ba5d48ba371a4f3
kibetkoech <vickibelangs@gmail.com>
2025-10-01 14:17:55 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  const textElement = document.getElementById("text");
+  if (textElement) {
+    textElement.textContent = "This is really cool!";
+  }
+});
